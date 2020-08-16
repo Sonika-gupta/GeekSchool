ae43ddfb93ca05f010041874a0ef3c5b4d83f982
Sonika Gupta <sonikagupta94@gmail.com>
2020-08-16 13:47:59 +0530

    removed parsing for object/array ending with ','

--- JSONparser.js
+++ JSONparser.js
@@ -7,8 +7,6 @@ const booleanParser = input => {
     if(input.startsWith('true')) return [true, input.slice(4)];
     if(input.startsWith('false')) return [false, input.slice(5)];
     return null;
-    // let matched = input.match(/^(true|false)/);
-    // return matched && [matched[0], input.slice(matched[0].length)];
 }
 
 const numberParser = input => {
@@ -22,13 +20,11 @@ const stringParser = input => {
 }
 
 const arrayParser = input => {
-    // let regex = /\[\s*(?<zero>[^,]*)(?:,(?<values>[^,]*))*\]/;
-    // let matched = input.match(/^\[(?:\s*|\s*([^,]+|\[.*\])(?:,([^,]+|\[.*\]))*)\]/);
-    let matched = input.match(/^\[.*\]/);
+    let matched = input.match(/^\[.*(?<!,)\s*\]/);
 
     if(!matched) return null;
 
-    matched[0] = matched[0].slice(1, matched[0].length-1).trim();
+    matched[0] = matched[0].slice(1, matched[0].length-1);
 
     let array = [], regex = new RegExp(/(\s*\[.*\]|{.*}|[^,\[{]+\s*(?=,|$))/g), value;
     while((value = regex.exec(matched[0])) !== null) {
@@ -40,8 +36,7 @@ const arrayParser = input => {
 }
 
 function objectParser (input) {
-    // let matched = input.match(/^{\s*(?:(("[^"]+")\s*:{.*}|\s*[^,]+)(?:,\s*(?:("[^"]+")\s*:{.*}|[^,]+)?)*)*}/);
-    let matched = input.match(/^{.*}/);
+    let matched = input.match(/^{.*(?<!,\s*)}/);
     if(!matched) return null;
     
     matched[0] = matched[0].slice(1, matched[0].length-1);
@@ -61,3 +56,28 @@ function objectParser (input) {
 function JSONParser (input) {
     return nullParser(input) || booleanParser(input) || numberParser(input) || stringParser(input) || arrayParser(input) || objectParser(input);
 }
+
+// let input = [
+//     "{  }",
+//     "{\"a:\" : 1}",
+//     "{\"asd\" : {}, \"asdf\": \"asd\", \"asf\": [1, 2, 3]  }",
+//     "[1, 2,3,[\"y\"]]",
+//     "[1,2,3,]",
+//     "\"asd\"",
+//     '"\ndgfd\""',
+//     '0asda',
+//     '0.123asda',
+//     '-0asda',
+//     '-0.0013asda',
+//     '-123.asda',
+//     '-123e-12.sas.34da',
+//     '-123e+12.sas.34da',
+//     '-123E1.sas.34da',
+//     '-123E.sas.34da',
+//     'asda12312ads3',
+//     'false12afas',
+//     'true12afas',
+//     'asdfalse',
+//     'nullabcd'
+// ];
+// input.forEach(el => console.log(JSONParser(el)));
